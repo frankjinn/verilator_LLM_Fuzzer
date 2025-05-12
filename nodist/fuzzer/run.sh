78d4d0d90940a4d78e7acd1ede24d59119e22600
Frank Jin <frank.jin@berkeley.edu>
2025-05-12 07:46:44 +0000

    reverting to smaller wrapper

--- nodist/fuzzer/run.sh
+++ nodist/fuzzer/run.sh
@@ -5,4 +5,4 @@
 
 # Actually do the fuzzing.  Note that this will not terminate in any reasonable
 # amount of time.  However, it will give updates on its progress.
-AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 AFL_SKIP_CPUFREQ=1 afl-fuzz -i in1 -o out1 -x dictionary ./wrapper --cc --timing --quiet --Wno-lint --Wno-fatal @@
+afl-fuzz -i in1 -o out1 -x dictionary ./wrapper --cc --timing --quiet --Wno-lint --Wno-fatal @@
--- nodist/fuzzer/wrapper.cpp
+++ nodist/fuzzer/wrapper.cpp
@@ -4,32 +4,79 @@
 #include <sys/wait.h>
 #include <cassert>
 
+// __AFL_FUZZ_INIT();
+
+// #define MAX_ITERATIONS 10000
+
+// int main(int argc, char** argv, char** envp) {
+//     // Initial cleanup should be outside the persistence loop
+//     int r = system("rm -rf obj_dir");
+//     assert(r == 0);
+
+//     #ifdef __AFL_HAVE_MANUAL_CONTROL
+//     __AFL_INIT();
+//     #endif
+
+//     // Get the fuzzing buffer before entering the persistence loop
+//     unsigned char *buf = __AFL_FUZZ_TESTCASE_BUF;
+
+//     // Main persistence loop
+//     while (__AFL_LOOP(MAX_ITERATIONS)) {
+//         // Create a new process for each iteration
+//         pid_t pid = vfork();
+        
+//         if (pid == -1) {
+//             _exit(1);
+//         }
+
+//         if (pid == 0) {
+//             // Child process
+//             execve("/verilator/bin/verilator", argv, envp);
+//             _exit(42);
+//         }
+
+//         // Parent process
+//         int status;
+//         if (waitpid(pid, &status, 0) == -1) {
+//             _exit(1);
+//         }
+
+//         // Clean up obj_dir after each iteration
+//         r = system("rm -rf obj_dir");
+//         assert(r == 0);
+
+//         // Reset any global state here if needed
+//     }
+
+//     return 0;
+// }
+
 int main(int argc, char** argv, char** envp) {
-    int r = system("rm -rf obj_dir");
+    auto r = system("rm -rf obj_dir");
     assert(r == 0);
-
-    pid_t pid = vfork(); // Use vfork() instead of fork() for better performance
-    if (pid == -1) {
-        perror("vfork failed");
-        _exit(1); // Use _exit() instead of abort() for faster termination
-    } 
-    
-    if (pid == 0) {
-        // Child process
-        execve("/verilator/bin/verilator", argv, envp);
-        _exit(42); // Remove perror to reduce syscalls
-    }
-
-    // Parent process
-    int status;
-    if (waitpid(pid, &status, 0) == -1) {
-        _exit(1); // Faster termination
-    }
-
-    // Simplified status check
-    if (WIFEXITED(status)) {
-        _exit(WEXITSTATUS(status) ? 1 : 0);
-    }
-    
-    _exit(1);
+    return execve("../../bin/verilator_bin", argv, envp);
 }
+
+// int main(int argc, char** argv, char** envp) {
+//     // Initial cleanup outside the persistence loop
+//     auto r = system("rm -rf obj_dir");
+//     assert(r == 0);
+
+//     #ifdef __AFL_HAVE_MANUAL_CONTROL
+//     __AFL_INIT();
+//     #endif
+
+//     while (__AFL_LOOP(10000)) {
+//         r = execve("../../bin/verilator_bin", argv, envp);
+//         if (r == -1) {
+//             perror("execve failed");
+//             _exit(1);
+//         }
+
+//         // Clean up between iterations
+//         r = system("rm -rf obj_dir");
+//         assert(r == 0);
+//     }
+
+//     return 0;
+// }
